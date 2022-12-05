e4474ce277c027c7220fb64fa78db6179a2fe25d
TalBenAtiya <benatia.tal@gmail.com>
2022-12-05 16:35:23 +0200

    minor route changes

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -9,8 +9,8 @@ function App() {
       <AppHeader />
       <main className='main-container'>
         <Routes>
-          <Route path='/*' element={<AppView />} >
-          <Route path='/*:projectName' element={<ProjectDetails />} />
+          <Route path='/' element={<AppView />} >
+          <Route path='/:projectName' element={<ProjectDetails />} />
           </Route>
         </Routes>
       </main>
