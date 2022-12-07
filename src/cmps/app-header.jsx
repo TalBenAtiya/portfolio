07ef5cf019e445d1a34b3cec5ce78796f0391cc4
TalBenAtiya <benatia.tal@gmail.com>
2022-12-07 11:47:41 +0200

    route minor change

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -26,7 +26,7 @@ export const AppHeader = () => {
     }
 
     const routeHome = () => {
-        navigate('/')
+        navigate('/portfolio')
         setIsMenuOpen(false)
     }
 
@@ -38,16 +38,16 @@ export const AppHeader = () => {
         <section className={"app-header" + (isScrolled ? " scrolled" : "")}>
             <header className="flex align-center main-layout main-header">
                 <div className="flex align-center logo-container">
-                    <a onClick={routeHome} href="/#my-portfolio" className="logo">
+                    <a onClick={routeHome} href="#my-portfolio" className="logo">
                         <img src={require("../assets/img/PortfolioLogo.png")} alt="Logo" />
                         <UpArrowSvg className={(isScrolled ? "scrolled" : "")} />
                     </a>
                 </div>
                 <nav className={isMenuOpen ? 'flex align-center open' : 'flex align-center'}>
-                    <a onClick={routeHome} href="/#services"><span><div className="box"></div> Services</span></a>
-                    <a onClick={routeHome} href="/#my-works"><span><div className="box"></div>My Works</span></a>
-                    <a onClick={routeHome} href="/#about"><span><div className="box"></div> About</span></a>
-                    <a onClick={routeHome} href="/#contact"><span><div className="box"></div> Contact</span></a>
+                    <a onClick={routeHome} href="#services"><span><div className="box"></div> Services</span></a>
+                    <a onClick={routeHome} href="#my-works"><span><div className="box"></div>My Works</span></a>
+                    <a onClick={routeHome} href="#about"><span><div className="box"></div> About</span></a>
+                    <a onClick={routeHome} href="#contact"><span><div className="box"></div> Contact</span></a>
                 </nav>
                 <div className="flex align-center">
                     <button onClick={toggleMenu} className={"hamburger"}>
